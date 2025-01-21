1e20e9fc9f86228dfc6c2d72ea24918a6c07f6bf
Jason Rieza <jasonrieza017@gmail.com>
2025-01-21 14:25:05 +0800

    Delete Sum directory

--- Sum/script.js
+++
@@ -1,8 +0,0 @@
-function toggleTheme() {
-    const html = document.documentElement;
-    const currentTheme = html.getAttribute('data-theme');
-    const newTheme = currentTheme === 'light' ? 'dark' : 'light';
-    html.setAttribute('data-theme', newTheme);
-}
-
-
